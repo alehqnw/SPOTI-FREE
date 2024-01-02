3ef4239cee3141bc8573d18d197a839b6b206ed9
alehqnw <ale120970@gmail.com>
2024-01-02 20:59:32 +0000

    *Importante* Scaffold implementado (no compila por algunos errores se mejorará en el futuro cuando esté todo hecho) y se vienen más cositas

--- app/src/main/java/com/example/proyectospotify/ui/pantalla/SongScreen.kt
+++ app/src/main/java/com/example/proyectospotify/ui/pantalla/SongScreen.kt
@@ -22,6 +22,7 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.unit.dp
 import com.example.proyectospotify.R
@@ -31,8 +32,8 @@ import com.example.proyectospotify.ui.views.SongViewModel
 @Composable
 fun SongScreen(viewModel: SongViewModel){
     // Obtenemos el estado de la canción desde el ViewModel
-    val songState by viewModel.songState.collectAsState()
-
+    val contexto = LocalContext.current
+    val songState:SongViewModel = SongViewModel()
     // Creamos una columna que ocupará todo el espacio disponible
     Column(
         modifier = Modifier.fillMaxSize(),
@@ -41,7 +42,7 @@ fun SongScreen(viewModel: SongViewModel){
     ) {
         // Mostramos el título de la canción
         Text(
-            text = songState.title,
+            text = "TITULo",
             style = MaterialTheme.typography.bodyLarge,
             modifier = Modifier.padding(bottom = 16.dp)
         )
@@ -55,25 +56,40 @@ fun SongScreen(viewModel: SongViewModel){
 
         // Mostramos una barra deslizante para controlar el progreso de la canción
         Slider(
-            value = songState.progress,
-            onValueChange = { viewModel.seekTo(it) },
-            valueRange = 0f..songState.duration.toFloat(),
+            value = songState.progreso.value.toFloat(),
+            onValueChange = { },
+            valueRange = 0f..songState.duracion.value.toFloat(),
             modifier = Modifier.padding(top = 16.dp, bottom = 32.dp)
         )
+        /*
+        * @Composable
+    fun YourComponent() {
+    val progreso = songState.progreso.collectAsState()
+    val duracion = songState.duracion.collectAsState()
 
+    Slider(
+       value = progreso.value.toFloat(),
+       onValueChange = { /* manejar el cambio de valor aquí */ },
+       valueRange = 0f..duracion.value.toFloat(),
+       modifier = Modifier.padding(top = 16.dp, bottom = 32.dp)
+    )
+    }
+
+        *
+        * */
         // Creamos una fila para los botones de control de reproducción
         Row(
             horizontalArrangement = Arrangement.SpaceEvenly,
             modifier = Modifier.fillMaxWidth()
         ) {
             // Botón para ir a la canción anterior
-            IconButton(onClick = { viewModel.previousSong() }) {
-                Icon(Icons.Filled.ArrowBack, contentDescription = null)
-            }
+            //IconButton(onClick = { viewModel.previousSong() }) {
+            //    Icon(Icons.Filled.ArrowBack, contentDescription = null)
+            //}
 
             // Botón para reproducir/pausar la canción
-            IconButton(onClick = { viewModel.togglePlayPause() }) {
-                if (songState.isPlaying) {
+            IconButton(onClick = { viewModel.PausarOSeguirMusica() }) {
+                if (songState.songState.value.isPlaying) {
                     Icon(Icons.Filled.Warning, contentDescription = null)
                 } else {
                     Icon(Icons.Filled.PlayArrow, contentDescription = null)
@@ -81,7 +97,7 @@ fun SongScreen(viewModel: SongViewModel){
             }
 
             // Botón para ir a la siguiente canción
-            IconButton(onClick = { viewModel.nextSong() }) {
+            IconButton(onClick = { viewModel.CambiarCancion(contexto) }) {
                 Icon(Icons.Filled.ArrowForward, contentDescription = null)
             }
         }
@@ -92,14 +108,14 @@ fun SongScreen(viewModel: SongViewModel){
             modifier = Modifier.fillMaxWidth()
         ) {
             // Botón para activar/desactivar el shuffle
-            IconButton(onClick = { viewModel.shuffle() }) {
+            //IconButton(onClick = { viewModel.shuffle() }) {
                 //Icon(Icons.Filled.Shuffle, contentDescription = null)
-            }
+            //}
 
             // Botón para activar/desactivar el repeat
-            IconButton(onClick = { viewModel.repeat() }) {
-                Icon(Icons.Filled.Refresh, contentDescription = null)
-            }
+            //IconButton(onClick = { viewModel.repeat() }) {
+            //    Icon(Icons.Filled.Refresh, contentDescription = null)
+            //}
         }
     }
 }
--- app/src/main/java/com/example/proyectospotify/ui/views/SongViewModel.kt
+++ app/src/main/java/com/example/proyectospotify/ui/views/SongViewModel.kt
@@ -1,20 +1,133 @@
 package com.example.proyectospotify.ui.views
 
+import android.content.ContentResolver
+import android.content.Context
+import android.content.res.Resources
+import android.media.browse.MediaBrowser
+import android.net.Uri
+import androidx.annotation.AnyRes
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import androidx.media3.common.MediaItem
+import androidx.media3.common.Player
 import androidx.media3.exoplayer.ExoPlayer
 import androidx.room.util.copy
 import com.example.proyectospotify.R
 import com.example.proyectospotify.ui.dataclass.Canciones
+import kotlinx.coroutines.delay
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.asStateFlow
+import kotlinx.coroutines.isActive
+import kotlinx.coroutines.launch
 import kotlin.random.Random
 
 class SongViewModel:ViewModel() {
     private val _songState: MutableStateFlow<ExoPlayer?> = MutableStateFlow(null)
     val songState = _songState.asStateFlow()
+    //Cancion actual
+    private val _actual = MutableStateFlow(R.raw.hellfire)
+    val actual = _actual.asStateFlow()
 
-    //
+    //Duracion de la cancion
+    private val _duracion = MutableStateFlow(0)
+    val duracion = _duracion.asStateFlow()
+
+    //Progreso actual de la cancion
+    private val _progreso = MutableStateFlow(0)
+    val progreso = _progreso.asStateFlow()
+
+    fun crearExo(context: Context){
+        _songState.value = ExoPlayer.Builder(context).build()
+        _songState.value!!.prepare()
+        _songState.value!!.playWhenReady = true
+    }
+    fun play(context:Context){
+        val mediaItem = MediaItem.fromUri(obtenerRuta(context,R.raw.hellfire))
+        _songState.value!!.setMediaItem(mediaItem)
+
+        _songState.value!!.addListener(object : Player.Listener{
+            override fun onPlaybackStateChanged(playbackState: Int) {
+                if(playbackState == Player.STATE_READY){
+                    // El Player está preparado para empezar la reproducción.
+                    // Si playWhenReady es true, empezará a sonar la música.
+
+                    _duracion.value = _songState.value!!.duration.toInt()
+
+                    viewModelScope.launch {
+                        /* TODO: Actualizar el progreso usando currentPosition cada segundo */
+                        while(isActive){
+                            _progreso.value = _songState.value!!.currentPosition.toInt()
+                            delay(1000)
+                        }
+
+                    }
+                }
+                else if(playbackState == Player.STATE_BUFFERING){
+                    //Meter lacancion para el buffering desgraciadamente no está listo pero, estamos en ello :)
+                }
+                else if(playbackState == Player.STATE_ENDED){
+                    CambiarCancion(context)
+                }
+                else if(playbackState == Player.STATE_IDLE){
+                    //El player se ha creado, pero no se ha lanzado la operacion prepared
+                }
+            }
+        }
+        )
+    }
+    // Este método se llama cuando el VM se destruya.
+    override fun onCleared() {
+        _songState.value!!.release()
+        super.onCleared()
+    }
+
+    fun PausarOSeguirMusica() {
+        /* TODO: Si el reproductor esta sonando, lo pauso. Si no, lo reproduzco */
+        if(!_songState.value!!.isPlaying ){
+            _songState.value!!.play()
+        }else{
+            _songState.value!!.pause()
+        }
+    }
+
+    fun CambiarCancion(context: Context) {
+
+        /* TODO: 1 - Cambiar la cancion actual y parar el mediaPlayer
+         *  2 - Limpiar al _exoPlayer de los mediaItems que tenga
+         *  3 - Crear mediaItem con la cancion actual
+         *  4 - Establecer dicho mediaItem
+         *  5 - Preparar el reproductor y activar el playWhenReady
+        */
+        if(_actual.value == R.raw.hellfire){
+            _actual.value = R.raw.extras
+        }else{
+            _actual.value = R.raw.hellfire
+        }
+
+        _songState.value!!.stop()
+        _songState.value!!.clearMediaItems()
+
+        val mediaItem = MediaItem.fromUri(obtenerRuta(context,_actual.value ))
+        _songState.value!!.setMediaItem(mediaItem)
+
+        _songState.value!!.prepare()
+        _songState.value!!.playWhenReady = true
+
+    }
+    @Throws(Resources.NotFoundException::class)
+    fun obtenerRuta(context: Context, @AnyRes resId: Int): Uri {
+        val res: Resources = context.resources
+        return Uri.parse(
+            ContentResolver.SCHEME_ANDROID_RESOURCE +
+                    "://" + res.getResourcePackageName(resId)
+                    + '/' + res.getResourceTypeName(resId)
+                    + '/' + res.getResourceEntryName(resId)
+        )
+    }
 
 }
+
+
+
